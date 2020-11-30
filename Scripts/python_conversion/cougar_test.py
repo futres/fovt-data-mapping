4a62958f08920172962c3703f2d974edf75f27f2
Neeka <nsewnath@ufl.edu>
2020-11-30 14:09:31 -0500

    updated py

--- Scripts/python_conversion/cougar_test.py
+++ Scripts/python_conversion/cougar_test.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """
 Author:  Neeka Sewnath <nsewnath@ufl.edu>
-Purpose: Clean Cougar Data
+Purpose: Clean Cougar Data (RShiny py functions example)
 """
+
+import argparse
+
 def get_args():
     """get command-line arguments"""
 
@@ -19,18 +22,53 @@ def get_args():
 
     return parser.parse_args()
 
+def clean_sex(sex):
+    """cleans sex column in dataset"""
+    #can catch incorrect information, needs testing
+    if sex.str.contains("female|F"):
+        sex = "female"
+    elif sex.str.contains("male|M"):
+        sex = "male"
+    else:
+        sex = "not collected"
+    return sex
+
+def clean_year(year):
+    """isolates year in dataset"""
+    #only works if year is in XX-XXXX or XX-XX-XXXX format
+    year = year.str[-4:]
+    return year
+
+def scientific_name(name):
+    """dictates scientific name"""
+    #sometimes scientific name is not included
+    name = ""
+    return name
+
+def clean_up(data):
+    """runs clean up function suite, keeps main clean"""
+    data["sex"] = data["sex"].apply(clean_sex)
+    data["yearCollected"] = data["yearCollected"].apply(clean_year)
+    data["scientificName"] = data["scientificName"].apply(scientific_name)
+
+#adding a universal country handler would be useful
+#checking discrepencies in lat and long values would be useful
+
+
+
 # --------------------------------------------------
 def main():
     """Read file and clean sex column"""
     
 args = get_args()
-female = data['Sex']=="F"
-male = data['Sex'] == "M"
-data['Sex'][(female == False)&(male==False)]="not collected"
-data['Sex'][female == True]="female"
-data['Sex'][male == True]="male"
+data = args
 
-return data
+#need a way to standardize column names before clean up functions
+#do we want to rename columns with regex?
+#needs to also initalize columns such as diagnostic ID
+
+
+clean_up(data)
 
 # --------------------------------------------------
 if __name__ == '__main__':
